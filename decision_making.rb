2f84a08497515e6f01184a2d4546dc14904afb77
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-08 16:36:46 -0700

    Answer questions for decision_making

--- decision_making.rb
+++ decision_making.rb
@@ -35,9 +35,20 @@ end
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+#### There is an if statement to determine the value that will be assigned to the variable bear_clothing
+#### If the variable door_choice is equal to 1 the string "hat" is assigned to bear_clothing, otherwise the
+#### string "scarf" is assigned to bear_clothing
 # 2. What variable has a new value assigned to it after the first if statement executes?
+#### brea_clothing
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+#### scarf
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+#### There is an if else statement to determine what action you will take. if bear_choice is equal to the integer
+#### 1, 2, or, 3 there is a different response for each, if brea_choice is any other integer
+#### "You stay with the bear and become it's best friend!" will be printed
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+#### "You run as fast as you can into the next room. It's full of snakes!" is output
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+#### "You tell the bear the hat is too small and it starts to cry!" is output
 # 7. What is your favorite ending?
+#### "You offer the bear your hat and the bear shows you a secret passage out!"
